1b21ce71770a357307c190862c9b64e0994e42eb
Nicholas Julia <nickajulia@gmail.com>
2015-05-26 14:54:51 -0700

    update to header text

--- leadpages-template/js/functions.js
+++ leadpages-template/js/functions.js
@@ -1,6 +1,6 @@
 // leadpages_input_data variables come from the template.json "variables" section
 var leadpages_input_data = {};
-var requestedHeight = 600;
 var requestedHeight = 3421;
 
 
+var myEventMethod = window.addEventListener ? "addEventListener" : "attachEvent";var myEventListener = window[myEventMethod];var myEventMessage = myEventMethod == "attachEvent" ? "onmessage" : "message";myEventListener(myEventMessage, function (e) {document.getElementById("myFrame").height = e.data + "px";}, false);
